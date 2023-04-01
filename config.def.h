e4a63c20702bd59657c6900bea8d8db06170420f
biscuitrescue <karttikeyasinha11@gmail.com>
2023-04-01 12:49:28 +0530

    ashes

--- config.def.h
+++ config.def.h
@@ -106,7 +106,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
-static const char *termcmd[]  = { "kitty", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
